09187a5fe1e82b1d74e75f7744ea57d43115fa20
geovanegter <48461670+geovanegter@users.noreply.github.com>
2025-11-06 16:58:59 -0300

    Update diario_bordo_app.py

--- diario_bordo_app.py
+++ diario_bordo_app.py
@@ -72,7 +72,10 @@ if not st.session_state.logado:
 # USUÁRIO LOGADO
 # -------------------------------
 user = st.session_state.user
-representante = user["email"]  # ou se tiver campo "representante"
+representante = user.get("nome", None)
+if representante is None:
+    st.error("❌ Usuário sem nome definido na planilha!")
+    st.stop()
 
 # -------------------------------
 # SIDEBAR
@@ -138,6 +141,7 @@ if not week_row.empty:
         clientes_restantes = max((percentual_meta / 100 * metas_colecao.loc[metas_colecao['colecao'] == row['colecao'], 'meta_vendas'].values[0]) / ticket_medio - vendas_semana / ticket_medio,0)
         st.markdown(f"Vendas semanais realizadas: R$ {vendas_semana:,.2f}")
         st.markdown(f"Clientes a atender nesta semana: {clientes_restantes:.0f}")
+
 
 
 
